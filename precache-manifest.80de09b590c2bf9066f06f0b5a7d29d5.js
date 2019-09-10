3c4e43d991d21bf6495d99675eaec119800168f2
Tran Quoc Khanh <khanhtran.cse@gmail.com>
2019-09-10 16:14:05 +0700

    Deploy

---
+++ precache-manifest.80de09b590c2bf9066f06f0b5a7d29d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "/my-viet-nam-admin/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "8ed3d0ad0d0fa7928437",
+    "url": "/my-viet-nam-admin/static/js/runtime~main.bda705f2.js"
+  },
+  {
+    "revision": "d87fb9add0c68c5b9538",
+    "url": "/my-viet-nam-admin/static/js/main.ffe0d78a.chunk.js"
+  },
+  {
+    "revision": "5520523076fbdb91297d",
+    "url": "/my-viet-nam-admin/static/js/2.dd4cd625.chunk.js"
+  },
+  {
+    "revision": "d87fb9add0c68c5b9538",
+    "url": "/my-viet-nam-admin/static/css/main.40ce7d97.chunk.css"
+  },
+  {
+    "revision": "5520523076fbdb91297d",
+    "url": "/my-viet-nam-admin/static/css/2.1a58d399.chunk.css"
+  },
+  {
+    "revision": "178149649a537d674e57fe13bc62def0",
+    "url": "/my-viet-nam-admin/index.html"
+  }
+];
